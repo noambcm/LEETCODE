7cf1c169c89a5f7d0742dd01275fb126cad7ebf1
noambcm <ouldbelkacemnoam@gmail.com>
2024-10-26 15:48:00 +0200

    26-10-2024

--- main.py
+++ main.py
@@ -4068,3 +4068,18 @@ class Solution:
             if candidat*puissance==s:
                 return True
         return False
+
+
+
+class Solution:
+    def maximumPrimeDifference(self, nums: List[int]) -> int:
+        prime=[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+        index=[]
+        for i,num in enumerate(nums):
+            if num in prime:
+                index.append(i)
+        return abs(index[0]-index[-1])
+
+
+
+        
