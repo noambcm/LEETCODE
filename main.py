ea7191036696e4ccc215e42abb935fb983643a5f
noambcm <ouldbelkacemnoam@gmail.com>
2025-01-07 16:32:14 +0100

    07-01-2024

--- main.py
+++ main.py
@@ -4916,3 +4916,13 @@ class Solution:
                     return False
         return True
 
+
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        old=0
+        for passenger in details:
+            age=int(passenger[11:13])
+            if age>60:
+                old+=1
+        return old
+
