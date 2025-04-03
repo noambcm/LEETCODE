c0ea2d16f81e5bc44cdeeed38f29fd406d3b7079
noambcm <ouldbelkacemnoam@gmail.com>
2025-04-03 10:54:06 +0200

    Code 03-04-2025

--- main.py
+++ main.py
@@ -5108,3 +5108,15 @@ class Solution:
         for key in keys:
             word += key*frequency[key]
         return word
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        answer = [0, 1]
+        for i in range(2,n+1):
+            answer.append(answer[i-1] + answer[i-2])
+        return answer[n]
