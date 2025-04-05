28ffeab211a751caf8d6d7b4076e2ff44a1abe27
noambcm <ouldbelkacemnoam@gmail.com>
2025-04-05 16:30:13 +0200

    Code 05/04/2025

--- main.py
+++ main.py
@@ -5149,4 +5149,18 @@ def minCostClimbingStairs(self, cost: List[int]) -> int:
         return min_cost(n)
 
 
+# Version 2 : mieux 
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0]*(n+1)
+        for i in range(2, n+1):
+            dp[i] = min(cost[i-1] + dp[i-1], cost[i-2] + dp[i-2])
+        return dp[n]
+
+
+
+
+
 
